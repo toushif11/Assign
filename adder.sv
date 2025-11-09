b3d2519f81393a5b21ff61e4629ae75c1dabf4dc
AKA <toushif@example.com>
2025-11-09 19:02:27 +0530

    Full Adder code is written in file adder.v

--- adder.sv
+++ adder.sv
@@ -2,3 +2,8 @@ module full_adder(input a, b, cin, output S, Cout);
   assign S = a ^ b ^ cin;
   assign Cout = (a & b) | (b & cin) | (a & cin);
 endmodule
+
+
+module tb_top;
+  reg a, b, c;
+  wire s, c_out;
